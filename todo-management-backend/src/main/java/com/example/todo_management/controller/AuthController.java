f55002b7a7d6f21eab79496456b7f51953605540
= <=>
2024-07-09 22:29:21 +0530

    jwt

--- todo-management-backend/src/main/java/com/example/todo_management/controller/AuthController.java
+++ todo-management-backend/src/main/java/com/example/todo_management/controller/AuthController.java
@@ -2,10 +2,14 @@ package com.example.todo_management.controller;
 
 import com.example.todo_management.dto.LoginDto;
 import com.example.todo_management.dto.RegisterDto;
+import com.example.todo_management.security.CustomUserDetails;
 import com.example.todo_management.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
     private AuthService authService;
+    private CustomUserDetails userDetails;
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
         String message = authService.register(registerDto);
@@ -23,5 +28,16 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         String message = authService.login(loginDto);
         return new ResponseEntity<>(message,HttpStatus.OK);
+    }
+    @GetMapping("/username")
+    public ResponseEntity<String> getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails user = (UserDetails) authentication.getPrincipal();
+            String username = userDetails.getUsername(user.getUsername());
+            return ResponseEntity.ok(username);
+        } else {
+            return ResponseEntity.ok(authentication.getName());
+        }
     }
 }
